41fc15d7b8921331b8cfd5968f5494a790fb89ce
Luis <alberto.gutierrezcetis138@gmail.com>
2024-07-19 04:11:15 -0600

    Conexión modificada

--- src/connection/connection.js
+++ src/connection/connection.js
@@ -12,7 +12,7 @@ export const conn = async () => {
     }
 
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect(process.env.MONGO_URI_ATLAS, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
