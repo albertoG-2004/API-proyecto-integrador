97bd590e4c1c42a932a48f7aa7108f134dbc4566
Luis <alberto.gutierrezcetis138@gmail.com>
2024-11-29 13:56:11 -0600

    Actualización de repositorio

--- server.js
+++ server.js
@@ -38,15 +38,15 @@ app.use("/users", routesUser);
 app.use("/bananas", routesBanana);
 app.use("/monitorings", routesMonitoring);
 
-const options = {
-    key: fs.readFileSync(process.env.RUTA_SSL_KEY),
-    cert: fs.readFileSync(process.env.RUTA_SSL_CERTIFICATE)
-};
+// const options = {
+//     key: fs.readFileSync(process.env.RUTA_SSL_KEY),
+//     cert: fs.readFileSync(process.env.RUTA_SSL_CERTIFICATE)
+// };
 
-https.createServer(options, app).listen(port, () => {
-    signale.success("Server running in port: " + port);
-});
-
-// app.listen(port, () => {
+// https.createServer(options, app).listen(port, () => {
 //     signale.success("Server running in port: " + port);
 // });
+
+app.listen(port, () => {
+    signale.success("Server running in port: " + port);
+});
--- src/services/sendDataService.js
+++ src/services/sendDataService.js
@@ -1,31 +1,14 @@
-import io from 'socket.io-client';
-import dotenv from 'dotenv';
+import io from "socket.io-client";
+import dotenv from "dotenv";
+
 dotenv.config();
 
-export const sendDataBananas = async(data) => {
-    const socket = io(process.env.SOCKET_SERVER);
-    const client = process.env.CLIENT;
+const socket = io(process.env.SOCKET_SERVER);
 
-    socket.on("connect", ()=>{
-        console.log("Connected to server");
-        socket.emit("bananas", data);
-    });
+export const sendDataBananas = (data) => {
+  socket.emit("bananas", data);
+};
 
-    socket.on("disconnect", ()=>{
-        console.log("Disconnect to server");
-    })
-}
-
-export const sendDataMonitorings = async(data) => {
-    const socket = io(process.env.SOCKET_SERVER);
-    const client = process.env.CLIENT;
-
-    socket.on("connect", ()=>{
-        console.log("Connected to server");
-        socket.emit("monitorings", data);
-    });
-
-    socket.on("disconnect", ()=>{
-        console.log("Disconnect to server");
-    })
-}
+export const sendDataMonitorings = (data) => {
+  socket.emit("monitorings", data);
+};
